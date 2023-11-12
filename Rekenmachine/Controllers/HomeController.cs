f57d873373d1ddb161fbdabd2372ffbc405811e5
Laurens8 <r0901293@student.thomasmore.be>
2023-11-12 16:04:52 +0100

    first commit

---
+++ Rekenmachine/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rekenmachine.Models;
+using System.Diagnostics;
+
+namespace Rekenmachine.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Bereken()
+        {
+            return View();
+        }
+
+        public IActionResult BerekenBoete()
+        {
+            return View();
+        }
+
+        [HttpPost("Bereken")]
+        public IActionResult Bereken(double num1, double num2, string bewerking)
+        {
+            double resultaat = 0;
+
+            switch (bewerking)
+            {
+                case "optellen":
+                    resultaat = num1 + num2;
+                    break;
+                case "aftrekken":
+                    resultaat = num1 - num2;
+                    break;
+                case "vermenigvuldigen":
+                    resultaat = num1 * num2;
+                    break;
+                case "delen":
+                    if (num2 != 0)
+                        resultaat = num1 / num2;
+                    else
+                        resultaat = double.NaN;
+                    break;
+            }
+
+            ViewData["Resultaat"] = resultaat;
+
+            return View();
+
+        }
+        public IActionResult NaamOmgekeerd()
+        {
+            return View();
+        }
+
+        [HttpPost("NaamOmgekeerd")]
+        public IActionResult NaamOmgekeerd(string naam)
+        {
+            char[] Tekenreeks = naam.ToCharArray();
+            Array.Reverse(Tekenreeks);
+            string omgekeerdeNaam = new(Tekenreeks);
+            ViewData["OmgekeerdeNaam"] = omgekeerdeNaam;
+            return View();
+        }
+
+        [HttpPost("BerekenBoete")]
+        public IActionResult BerekenBoete(int gemetenSnelheid)
+        {
+            int maximaleSnelheid = 50;
+
+            int vastGedeelte = 125;
+
+            int variabelGedeeltePerKm = 25;
+
+            if (gemetenSnelheid > maximaleSnelheid)
+            {
+                int teHardGereden = gemetenSnelheid - maximaleSnelheid;
+                int variabelGedeelte = 0;
+
+                while (teHardGereden > 0)
+                {
+                    variabelGedeelte += variabelGedeeltePerKm;
+                    teHardGereden--;
+                }
+
+                int totaleBoete = vastGedeelte + variabelGedeelte;
+
+                if (gemetenSnelheid > 50)
+                {
+                    ViewData["Resultaat"] = "Foei, u krijgt een boete van €" + totaleBoete;
+                }
+                else
+                {
+                    ViewData["Resultaat"] = "Proficiat, u bent een van de weinige die niet te hard rijdt!";
+                }
+                
+
+                return View();
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+    }
+
+}
